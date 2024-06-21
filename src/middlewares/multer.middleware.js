5ecd704f2eba1900e2769264271983ac02666b9e
sumdahl <sumirandahal46@gmail.com>
2024-06-21 15:53:30 +0545

    multer pushed

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,14 @@
+import multer from "multer";
+import nanoid from "nanoid";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp");
+  },
+  filename: function (req, file, cb) {
+    const filename = nanoid(10);
+    cb(null, `${file.fieldname}-${filename}`);
+  },
+});
+
+export const upload = multer({ storage });
