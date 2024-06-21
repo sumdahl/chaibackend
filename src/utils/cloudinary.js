58bf823b64b76c6d1041303988c0cd10d76a4f85
sumdahl <sumirandahal46@gmail.com>
2024-06-21 15:01:48 +0545

    cloudinary setup and multer middleware

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,36 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) {
+      console.error("Could not find the filepath");
+      return null;
+    } else {
+      const uploadResult = await cloudinary.uploader.upload(localFilePath, {
+        resource_type: "auto",
+      });
+      console.log("File uploaded successfully: ", uploadResult.url);
+      return uploadResult;
+    }
+  } catch (uploadError) {
+    console.error("Error uploading files: ", uploadError);
+    try {
+      if (localFilePath) {
+        fs.unlinkSync(localFilePath); //remove the locally saved temp file as the upload operation got failed
+        console.log("Temporary file removed:", localFilePath);
+      }
+    } catch (removeError) {
+      console.error("Failed to remove temporary file:", removeError);
+    }
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
