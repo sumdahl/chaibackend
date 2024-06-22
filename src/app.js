32c1be2a75b2551979a9f9873e0aeb591de30b0b
sumdahl <sumirandahal46@gmail.com>
2024-06-22 12:04:22 +0545

    project configuration , routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express, { urlencoded } from "express";
 import cors from "cors";
-import { MAX_SIZE } from "./constants";
+import { MAX_SIZE } from "./constants.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
@@ -16,10 +16,17 @@ app.use(
   express.json({
     limit: MAX_SIZE,
   })
-);
+);  
 app.use(express.urlencoded({ extended: true, limit: MAX_SIZE }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+
+//Routes import
+import userRouter from "./routes/user.routes.js"
+
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
 
 export { app };
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ dotenv.config({
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server is running at port ${process.env.port}`);
+      console.log(`Server is running at port ${process.env.PORT}`);
     });
     app.on("error", (error) => {
       console.log("ERROR: ", error);
