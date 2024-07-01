5855b5666a882f98ab334428d2e90770664b3e70
sumdahl <sumirandahal46@gmail.com>
2024-07-01 14:02:02 +0545

    add refreshtoken endpoint

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -20,7 +20,6 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
     );
   
     if (!user) {
-      //TODO; discuss about frontend
       throw new ApiError(401, "Invalid access token.");
     }
   
